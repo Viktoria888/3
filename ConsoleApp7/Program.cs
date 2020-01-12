5802218d05171a7cb2053efb90f86a7a947a0399
nata-vika@list.ru <nata-vika@list.ru>
2020-01-12 15:27:36 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"сотые: {num / 100}");
+            Console.WriteLine($"десятки: {num / 10 % 10}");
+            Console.WriteLine($"единицы: {num % 10}");
+            Console.ReadKey();
+        }
+    }
+}
